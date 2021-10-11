f8b2d0aeca2a0ee2fdee6bde595ad1d1d39c3760
liabrooke <88808863+liabrooke@users.noreply.github.com>
2021-10-10 20:39:17 -0500

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,64 @@
+``` Module 7 Challenege - code saved from PGAdmin
+
+-- Retrieve employee info
+SELECT emp_no, 
+       first_name, 
+	   last_name,
+FROM employees;
+
+-- Retreive titles info
+SELECT title,
+	   from_date,
+	   to_date
+FROM titles;
+
+-- Joining Employees and Titles Table
+SELECT e.emp_no,
+       e.first_name, 
+	   e.last_name,
+	   t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY t.emp_no, e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Number of employees about to retire sorted by job title 
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+-- Deliverable 2
+
+-- Joining Employees, Department Employees, and Titles Table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+       e.first_name, 
+	   e.last_name,
+	   e.birth_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t 
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
